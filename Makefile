3ac57bbd8b9c6e1945b39f6885758fbbb831dba3
dkozak <dkozak94@gmail.com>
2016-11-23 16:20:05 +0100

    add Makefile

---
+++ Makefile
@@ -0,0 +1,38 @@
+
+ARCHIVE=04_xkozak15-xmikla10.tar.gz
+
+#compiler
+CC=g++
+
+#optimization
+OPT=-O2
+
+#dynamically linked libraries
+libs=-lsimlib -lm
+
+#flags
+CPPFLAGS= -std=c++11 -Wall -Wextra -pedantic -pthread
+
+#name of the executable
+BASIC_MODEL=basic
+
+OBJECT_FILES=basic_model.o
+FILES=basic_model.cpp
+
+
+.PHONY: build
+.PHONY: clean
+.PHONY: distrib
+
+build: ${BASIC_MODEL}
+
+${BASIC_MODEL}: ${FILES_FULL_PATH}
+	${CC} ${CPPFLAGS} ${FILES} -o ${BASIC_MODEL} ${libs} ${OPT}
+
+
+distrib:
+	tar -cvzf ${ARCHIVE} ${FILES} Makefile dokumentace.pdf
+
+clean:
+	rm -f ${OBJECT_FILES} ${BASIC_MODEL}
+	rm -f ${ARCHIVE}
--- basic_model.cpp
+++ basic_model.cpp
@@ -1,6 +1,42 @@
 #include <iostream>
+#include "simlib.h"
+
+
+// TODO FIX CMAKE, use Makefile for now
+
+/**
+ *          CONSTANTS
+ * All constants are in seconds
+ */
+
+/**
+ * We are modelling three hours
+ */
+static const int SIMULATION_END_TIME =  60*60*3;
+static const int CUSTUMER_CREATION_TIME = 10;
+static const int GATE_ENTERING_TIME = 3;
+
+Facility gate("Gate");
+
+class Customer : public Process{
+    void Behavior(){
+        Seize(gate);
+        Wait(GATE_ENTERING_TIME);
+        Release(gate);
+    }
+};
+
+class Generator: public Event{
+    void Behavior(){
+        (new Customer)->Activate();
+        Activate(Time + Exponential(CUSTUMER_CREATION_TIME));
+    }
+};
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    Init(0,SIMULATION_END_TIME);
+    (new Generator)->Activate();
+    Run();
+    gate.Output();
     return 0;
 }
